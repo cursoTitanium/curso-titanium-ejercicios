5dc585375bc6851c53fca80441dc035ee68bcb83
Jorge Macías <jormagar@upvnet.upv.es>
2016-11-30 00:54:38 +0100

    Enunciados ejercicios

--- app/controllers/tema1/ejercicio7.js
+++ app/controllers/tema1/ejercicio7.js
@@ -1,2 +1,15 @@
-// Arguments passed into this controller can be accessed via the `$.args` object directly or:
-var args = $.args;
+/**
+	EJERCICIO 7: SCROLL VIEW
+	
+	Dada la serie de vistas distribuidas en la pantalla se deberá buscar
+	una solución para poder permitir el desplazamiento sobre ellas y poder
+	ver todo el contenido disponible.
+	
+	Utilizará IDs y clases guardándolo todo en el estilo asociado al controlador ejercicio 7:
+	
+	app/styles/tema1/ejercicio7.tss
+	
+	Ayuda:
+	http://docs.appcelerator.com/platform/latest/#!/guide/Layouts,_Positioning,_and_the_View_Hierarchy
+	http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.ScrollView
+ */
