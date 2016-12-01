fbe4c52a82aaf440d2fd601151fd325206214e2c
Jorge Macías <jormagar@upvnet.upv.es>
2016-12-01 02:06:10 +0100

    Ejercicios Tema 2 Parte 1

---
+++ app/controllers/tema2/ejercicio1.js
@@ -0,0 +1,37 @@
+(function constructor(args) {
+	"use strict";
+
+	/**
+	 * setEventos
+	 * @description Añadimos eventos a los elementos de UI
+	 */
+	function setEventos() {
+		//Añadimos evento al botón enviar y asignamos callback
+		
+		//Añadimos evento al botón reset y asignamos callback
+	}
+
+	/**
+	 * validarCamposRequeridos
+	 * @description Validamos campos requeridos
+	 * @param {Object} e
+	 */
+	function validarCamposRequeridos(e) {
+		//Implementar función de validación de campos requeridos
+		//Nombre, email y mensaje
+		
+		//Si validación correcta mensaje de alerta "Mensaje enviado."
+		//Si error, marcar campos erróneos
+	}
+
+	/**
+	 * resetFormulario
+	 * @description Vaciamos los campos del formulario
+	 * @param {Object} e
+	 */
+	function resetFormulario(e) {
+		//Implementar función de validación de campos requeridos
+		//Nombre, email y mensaje
+	}
+
+})($.args || {});
---
+++ app/controllers/tema2/ejercicio2.js
@@ -0,0 +1,25 @@
+(function constructor(args) {
+	"use strict";
+
+	/**
+	 * setEventos
+	 * @description Añadimos eventos a los elementos de UI
+	 */
+	function setEventos() {
+		//Añadimos eventos de click a los elementos del menú y su callback
+	}
+
+	/**
+	 * clickEnMenu
+	 * @description Callback cuando se pulsa en un item de menu
+	 * @param {Object} e
+	 */
+	function clickEnMenu(e) {
+		//Implementar función que asigna una clase activa al menú pulsado
+		//Y deja el resto como inactivas
+
+		//Añadir contenido a vista contenedor. Si ya hay una vista en el contenedor
+		//debemos eliminarla antes para añadir la nueva
+	}
+
+})($.args || {});
