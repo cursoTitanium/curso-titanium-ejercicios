f622b2086a7b049741eb9825f7b02b8c508b7765
Jorge Macías <jormagar@upvnet.upv.es>
2016-12-13 19:28:32 +0100

    Ejercicios tema 4

---
+++ app/controllers/tema4/ejercicio1.js
@@ -0,0 +1,12 @@
+(function constructor(args) {
+	"use strict";
+
+	/**
+	 * setEventos
+	 * @description Añadimos eventos a los elementos de UI
+	 */
+	(function setEventos() {
+
+	})();
+
+})($.args || {}); 
